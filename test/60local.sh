7a7781f2faf002f4f4d7db96cae190c4e25868c6
Kevin Chevreuil <kevinchevreuil@protonmail.com>
2025-08-13 00:13:55 +0200

    Import the 0.198 release

--- test/60local.sh
+++ test/60local.sh
@@ -11,6 +11,16 @@ oneTimeSetUp() {
 
 setUp() {
     rm -f "${sourceslist}"
+
+    # clear out settings between tests
+    for x in _DEVEL_ local0 local1; do
+        for y in source repository comment key; do
+            unset apt_setup_${x}_${y}
+        done
+    done
+    unset db_go_ret
+
+    # provide a default fetch result, that can be overriden
     export fetch_url_result="pretend binary GPG key"
 }
 
@@ -40,15 +50,33 @@ test_comment() {
     assertTrue "'${sourceslist}' should contain '${apt_setup_local0_repository}'" "grep -q '${apt_setup_local0_repository}' '${sourceslist}'"
 }
 
-test_key() {
+test_options() {
     touch "$ROOT/tmp/key0.pub"
     apt_setup_local0_repository="http://some.url/"
-    apt_setup_local0_key=xxxx
+    apt_setup_local0_options="check-valid-until=false"
     output=$(. $SCRIPT "${sourceslist}" 2>&1)
     result=$?
     assertTrue "command failed with this output: '$output'" "[ 0 -eq $result ]"
     assertTrue "'${sourceslist}' should have been created" "[ -e '${sourceslist}' ]"
     assertTrue "'${sourceslist}' should contain '${apt_setup_local0_repository}'" "grep -q '${apt_setup_local0_repository}' '${sourceslist}'"
+    assertTrue "'${sourceslist}' should contain '${apt_setup_local0_options}'" "grep -q '${apt_setup_local0_options}' '${sourceslist}'"
+    # head -v "${sourceslist}"
+}
+
+test_options_and_key() {
+    touch "$ROOT/tmp/key0.pub"
+    apt_setup_local0_repository="http://some.url/"
+    apt_setup_local0_options="check-valid-until=false"
+    apt_setup_local0_comment="some_comment"
+    apt_setup_local0_key=zzzz # we're mocking fetch_url, so this setting isn't actually used
+    output=$(. $SCRIPT "${sourceslist}" 2>&1)
+    result=$?
+    assertTrue "command failed with this output: '$output'" "[ 0 -eq $result ]"
+    assertTrue "'${sourceslist}' should have been created" "[ -e '${sourceslist}' ]"
+    assertTrue "'${sourceslist}' should contain '${apt_setup_local0_repository}'" "grep -q '${apt_setup_local0_repository}' '${sourceslist}'"
+    assertTrue "'${sourceslist}' should contain 'signed-by=/etc/apt/keyrings/${apt_setup_local0_comment}.gpg'" "grep -q 'signed-by=/etc/apt/keyrings/${apt_setup_local0_comment}.gpg' '${sourceslist}'"
+    assertTrue "'${ROOT}/etc/apt/keyrings/${apt_setup_local0_comment}.gpg' should contain '${fetch_url_result}'" "grep -q '${fetch_url_result}' '${ROOT}/etc/apt/keyrings/${apt_setup_local0_comment}.gpg'"
+    assertTrue "'${sourceslist}' should contain '${apt_setup_local0_options}'" "grep -q '${apt_setup_local0_options}' '${sourceslist}'"
 }
 
 test_source() {
@@ -66,7 +94,7 @@ test_gpg_asc() {
     touch "$ROOT/tmp/key0.pub"
     apt_setup_local0_repository="http://some.url/"
     apt_setup_local0_source=true
-    export fetch_url_result='-----BEGIN PGP PUBLIC KEY BLOCK-----'
+    fetch_url_result='-----BEGIN PGP PUBLIC KEY BLOCK-----'
 
     output=$(. $SCRIPT "${sourceslist}" 2>&1)
     result=$?
@@ -79,7 +107,11 @@ test_failed_fetch() {
     touch "$ROOT/tmp/key0.pub"
     apt_setup_local0_repository="http://some.url/"
     apt_setup_local0_source=true
-    export fetch_url_result='fail'
+    apt_setup_local0_key=zzzz # we're mocking fetch_url, so this setting isn't actually used
+    # simulate a failure of fetch_url
+    fetch_url_result='fail'
+    # make it look like the user doesn't want to retry
+    db_go_ret=false
 
     output=$(. $SCRIPT "${sourceslist}" 2>&1)
     result=$?
@@ -92,9 +124,12 @@ test_ignore_failed_fetch() {
     touch "$ROOT/tmp/key0.pub"
     apt_setup_local0_repository="http://some.url/"
     apt_setup_local0_source=true
-    export fetch_url_result='fail'
-    apt_setup_local_key_error=Ignore
+    apt_setup_local0_key=zzzz # we're mocking fetch_url, so this setting isn't actually used
+    # simulate a failure of fetch_url
+    fetch_url_result='fail'
+    # make it look like the user wants to ignore the failure
     db_go_ret=true
+    apt_setup_local_key_error=Ignore
 
     output=$(. $SCRIPT "${sourceslist}" 2>&1)
     result=$?
@@ -108,7 +143,7 @@ test_multi_repo() {
     apt_setup_local0_repository="http://some.url/"
     apt_setup_local0_source=true
     apt_setup_local1_repository="https://someother.url/"
-    export fetch_url_result='fail'
+    fetch_url_result='fail'
     apt_setup_local_key_error=Ignore
     db_go_ret=true
 
